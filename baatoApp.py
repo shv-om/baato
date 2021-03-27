5b44da8b6bb820e86fffa846f9d09b3ff87e0c6c
Ravi Prakash <1907prakash.ravi@gmail.com>
2021-03-27 23:42:42 +0530

    adding app file

---
+++ baatoApp.py
@@ -0,0 +1,38 @@
+import socket_server as sender
+import socket_client as receiver
+
+def menu():
+    mainMenu = "Menu\n"+"-"*6+"\n\n1. Send Files! \n2. Receive Files!"
+
+    print(mainMenu)
+    ch = input("Enter your choice: ")
+    
+    return ch
+
+def app():
+    choice = menu()
+
+    ## making host to share files
+    if choice == '1':
+        Sender = sender.socket_server()
+        Sender.server()
+        return True
+
+    ## making client to get files
+    elif choice == '2':
+        Receiver = receiver.socket_client()
+        Receiver.client()
+        return True
+
+    ## invalid input
+    else:
+        print("Something went wrong!!")
+        return False
+
+################
+
+if __name__ == '__main__':
+    flag = True
+    while flag:
+        flag = app()
+    print("\nThanks for using Baato!!\n")
